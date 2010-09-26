bc3b43dae3d307264b8fa19d4b3493a8226a1741
Nelson Luis Rodrigues Pereira <nelson.ufs@gmail.com>
2010-09-26 11:48:22 -0300

    Criando os filtros de aluno

---
+++ lib/filter/doctrine/StudentFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Student filter form.
+ *
+ * @package    academico
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class StudentFormFilter extends BaseStudentFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/filter/doctrine/base/BaseStudentFormFilter.class.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Student filter form base class.
+ *
+ * @package    academico
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseStudentFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'name'    => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'email'   => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'user_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('sfGuardUser'), 'add_empty' => true)),
+    ));
+
+    $this->setValidators(array(
+      'name'    => new sfValidatorPass(array('required' => false)),
+      'email'   => new sfValidatorPass(array('required' => false)),
+      'user_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('sfGuardUser'), 'column' => 'id')),
+    ));
+
+    $this->widgetSchema->setNameFormat('student_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Student';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'      => 'Number',
+      'name'    => 'Text',
+      'email'   => 'Text',
+      'user_id' => 'ForeignKey',
+    );
+  }
+}

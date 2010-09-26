9ce48f67af0be92b654560249f7ead4e1043df1f
Nelson Luis Rodrigues Pereira <nelson.ufs@gmail.com>
2010-09-26 20:52:50 -0300

    Criando o módulo student

---
+++ apps/academico/modules/student/lib/studentGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * student module configuration.
+ *
+ * @package    academico
+ * @subpackage student
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class studentGeneratorConfiguration extends BaseStudentGeneratorConfiguration
+{
+}

3a024ef04de91d48bd3cbf9204aa7e0dbfd1c5cb
Nelson Luis Rodrigues Pereira <nelson.ufs@gmail.com>
2010-09-26 23:22:23 -0300

    Adicionando a configuração do user no form de student

--- lib/form/doctrine/StudentForm.class.php
+++ lib/form/doctrine/StudentForm.class.php
@@ -12,5 +12,25 @@ class StudentForm extends BaseStudentForm
 {
   public function configure()
   {
+      unset($this['user_id']);
+      $this->widgetSchema['username'] = new sfWidgetFormInputText();
+      $this->validatorSchema['username'] = new sfValidatorString(array('required' => true));
+      $this->widgetSchema['password'] = new sfWidgetFormInputPassword();
+      $this->validatorSchema['password'] = new sfValidatorString(array('max_length' => 128, 'required' => false));
+      $this->widgetSchema['password_again'] = new sfWidgetFormInputPassword();
+      $this->validatorSchema['password_again'] = clone $this->validatorSchema['password'];
+      $this->widgetSchema->moveField('password_again', 'after', 'password');
+      $this->mergePostValidator(new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_again', array(), array('invalid' => 'The two passwords must be the same.')));
+      $this->validatorSchema->setOption('allow_extra_fields', true);
+  }
+
+  public function  save($con = null) {
+      $sf_guard_user = new sfGuardUser();
+      $sf_guard_user->setUsername($this->taintedValues['username']);
+      $sf_guard_user->setPassword($this->taintedValues['password']);
+      $sf_guard_user->setIsActive();
+      $sf_guard_user->save();
+      $this->getObject()->setUserId($sf_guard_user->getId());
+      parent::save($con);
   }
 }

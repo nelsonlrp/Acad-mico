ef77ecb1a2ada7b9ba87302fa4b8a19c4ddf7a96
Nelson Luis Rodrigues Pereira <nelson.ufs@gmail.com>
2010-09-28 20:23:00 -0300

    Modificando o form de student

--- lib/form/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardUserForm.class.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardUserForm.class.php
@@ -51,10 +51,7 @@ abstract class BasesfGuardUserForm extends BaseFormDoctrine
     ));
 
     $this->validatorSchema->setPostValidator(
-      new sfValidatorAnd(array(
-        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('email_address'))),
-        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('username'))),
-      ))
+      new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('username')))
     );
 
     $this->widgetSchema->setNameFormat('sf_guard_user[%s]');

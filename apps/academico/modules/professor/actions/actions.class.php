71e968aa3ff6a4442cbba69924101aaeca0f30e6
Nelson Luis Rodrigues Pereira <nelson.ufs@gmail.com>
2010-10-09 13:03:33 -0300

    Criando o módulo professor

---
+++ apps/academico/modules/professor/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/professorGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/professorGeneratorHelper.class.php';
+
+/**
+ * professor actions.
+ *
+ * @package    academico
+ * @subpackage professor
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class professorActions extends autoProfessorActions
+{
+}
---
+++ apps/academico/modules/professor/lib/professorGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * professor module helper.
+ *
+ * @package    academico
+ * @subpackage professor
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class professorGeneratorHelper extends BaseProfessorGeneratorHelper
+{
+}

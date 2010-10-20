10cf42c8795485f57ee38b1f69b29083d3e32580
Nelson Luis Rodrigues Pereira <nelson.ufs@gmail.com>
2010-10-19 22:58:24 -0300

    Configuração do form de professor

--- lib/form/doctrine/ProfessorForm.class.php
+++ lib/form/doctrine/ProfessorForm.class.php
@@ -12,5 +12,33 @@ class ProfessorForm extends BaseProfessorForm
 {
   public function configure()
   {
+      unset($this['user_id']);
+      $this->validatorSchema['email'] = new sfValidatorAnd(array(
+          $this->validatorSchema['email'],
+          new sfValidatorEmail(),
+        ));
+      $this->widgetSchema['username'] = new sfWidgetFormInputText();
+      $this->widgetSchema['username']->setAttribute('value', $this->getObject()->getSfGuardUser()->getUsername());
+      $this->validatorSchema['username'] = new sfValidatorString(array('required' => true));
+      $this->widgetSchema['password'] = new sfWidgetFormInputPassword();
+      $this->validatorSchema['password'] = new sfValidatorString(array('max_length' => 128, 'required' => false));
+      $this->widgetSchema['password_again'] = new sfWidgetFormInputPassword();
+      $this->validatorSchema['password_again'] = clone $this->validatorSchema['password'];
+      $this->widgetSchema->moveField('password_again', 'after', 'password');
+      $this->mergePostValidator(new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_again', array(), array('invalid' => 'The two passwords must be the same.')));
+      $this->validatorSchema->setOption('allow_extra_fields', true);
+  }
+
+  public function  save($con = null) {
+      $sf_guard_user = new sfGuardUser();
+      $name = explode(" ", $this->taintedValues['name']);
+      $this->getObject()->getSfGuardUser()->setFirstName($name[0]);
+      $this->getObject()->getSfGuardUser()->setLastName($name[count($name)-1]);
+      $this->getObject()->getSfGuardUser()->setUsername($this->taintedValues['username']);
+      $this->getObject()->getSfGuardUser()->setPassword($this->taintedValues['password']);
+      $this->getObject()->getSfGuardUser()->setEmailAddress($this->taintedValues['email']);
+      $this->getObject()->getSfGuardUser()->setIsActive();
+      parent::save($con);
+      return $this->getObject();
   }
 }
